90d2aac9bedd3100dee93f6c29c16f75891564b7
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2021-03-25 22:00:09 +0000

    add skeleton partprobe interface (not doing anything yet)

---
+++ web/html/partprobe.php
@@ -0,0 +1,26 @@
+<div class="row">
+  <div class="twelve columns">
+    <h3>Low-level partprobe...</h3>
+  </div>
+</div>
+<div class="row">
+  <div class="ten columns">
+    <div id="progress_bg">
+      <div id="progress">
+        <div id="progress_bar">0%</div>
+      </div>
+      <p id="status">
+        Not implemented yet.
+      </p>
+    </div>
+  </div>
+  <div class="two columns">
+    <form method="post">
+      <fieldset>
+        <input id="back_button2" type="submit" value="Back to main page" />
+      </fieldset>
+    </form>
+  </div>
+//updateProgress();
+  </script>
+</div>    
